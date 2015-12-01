92990a42201da2d71ab079303158481cc669da91
salami350 <salami350@live.nl>
2015-12-01 13:05:04 +0100

    made some advances in the implementation

--- JSF31KochFractalFX - startup/src/calculate/KochManagerWorker.java
+++ JSF31KochFractalFX - startup/src/calculate/KochManagerWorker.java
@@ -5,10 +5,13 @@
  */
 package calculate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  *
@@ -19,20 +22,33 @@ public class KochManagerWorker
     private CyclicBarrier cyclicBarrier;
     private KochFractal kochFractal;
     private ExecutorService pool;
-    
+        
     public KochManagerWorker(KochFractal kochFractal)
     {
         this.kochFractal = kochFractal;
         
         pool = Executors.newFixedThreadPool(3);
         cyclicBarrier = new CyclicBarrier(3);
+        
     }
     
-    public void ChangeLevel()
+    public List<Edge> ChangeLevel() throws InterruptedException
     {
-        for (int i = 1; i <= 3; i++)
+        KochWorker kochWorkerLeft = new KochWorker(1, kochFractal, cyclicBarrier);
+        KochWorker kochWorkerBottom = new KochWorker(2, kochFractal, cyclicBarrier);
+        KochWorker kochWorkerRight = new KochWorker(3, kochFractal, cyclicBarrier);
+        
+        Future futureLeft = pool.submit(kochWorkerLeft);
+        Future futureBottom = pool.submit(kochWorkerBottom);
+        Future futureRight = pool.submit(kochWorkerRight);
+        
+        while(!futureLeft.isDone() || !futureBottom.isDone() || futureRight.isDone())
         {
-            pool.submit(new KochWorker(i, kochFractal, cyclicBarrier));
+            System.out.println("Tasks are not complete yet...");
+            Thread.sleep(1);
         }
+        
+        List<Edge> edges = new ArrayList<>();
+        
     }
 }
